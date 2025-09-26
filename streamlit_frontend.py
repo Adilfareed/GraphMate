50e8f028b66c01bd0cd310255161a8894835ff63
Adil fareed <82055942+Adilfareed@users.noreply.github.com>
2025-09-26 19:44:10 +0500

    add streaming feature

--- streamlit_frontend.py
+++ streamlit_frontend.py
@@ -2,7 +2,7 @@ import streamlit as st
 from langgraph_backend import chatbot
 from langchain_core.messages import HumanMessage
 
-
+st.title("Graph Mate")
 CONFIG = {"configurable": {"thread_id": "thread-1"}}
 
 with st.chat_message('assistant'):
@@ -30,16 +30,25 @@ if user_input:
         st.text(user_input)
 
 
-    response = chatbot.invoke(
-        {"messages": [HumanMessage(content=user_input)]}, config=CONFIG
-    )
+    # response = chatbot.invoke(
+    #     {"messages": [HumanMessage(content=user_input)]}, config=CONFIG
+    # )
 
-    ai_message = response["messages"][-1].content
+    # ai_message = response["messages"][-1].content
 
     # Save AI response
+   
+    with st.chat_message("assistant"):
+        
+        ai_message=st.write_stream(
+            message_chunk.content for message_chunk , metadat in chatbot.stream(
+                {'messages':[HumanMessage(content=user_input)],},
+                config=CONFIG,
+                stream_mode="messages"
+
+            )
+        )
+
     st.session_state["message_history"].append(
         {"role": "assistant", "content": ai_message}
     )
-
-    with st.chat_message("assistant"):
-        st.text(ai_message)
